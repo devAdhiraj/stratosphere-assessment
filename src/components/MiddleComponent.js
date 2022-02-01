4814c78b78d7276120aed3bba1893b819cc15f3e
ryanwhitemedia <hello@ryanwhitemedia.com>
2022-02-01 09:39:29 -0500

    Add comments

--- src/components/MiddleComponent.js
+++ src/components/MiddleComponent.js
@@ -1,55 +1,67 @@
-import React from 'react';
-import {ReactComponent as GreenIcon } from '../assets/green-icon.svg';
-import {ReactComponent as YellowIcon } from '../assets/yellow-icon.svg';
-import {ReactComponent as BlueIcon } from '../assets/blue-icon.svg';
-import {ReactComponent as OrangeIcon } from '../assets/orange-icon.svg';
-import {ReactComponent as PurpleIcon } from '../assets/purple-icon.svg';
-import {ReactComponent as RedIcon } from '../assets/red-icon.svg';
+import React from "react";
+// import { ReactComponent as GreenIcon } from "../assets/green-icon.svg";
+// import { ReactComponent as YellowIcon } from "../assets/yellow-icon.svg";
+// import { ReactComponent as BlueIcon } from "../assets/blue-icon.svg";
+// import { ReactComponent as OrangeIcon } from "../assets/orange-icon.svg";
+// import { ReactComponent as PurpleIcon } from "../assets/purple-icon.svg";
+// import { ReactComponent as RedIcon } from "../assets/red-icon.svg";
+import { ReactComponent as GlobalIcon } from "../assets/global-icon.svg";
 import styles from "./MiddleComponent.module.css";
 
-const MiddleComponent = ({data}) => {
-    return (
-        <section className={styles.componentWrapper}>
-            <div className={styles.container}>
-                <div className={styles.elemWrapper}>
-                    <BlueIcon />
-                    <p>
-                        {data.blueText}
-                    </p>
-                </div>
-                <div className={styles.elemWrapper}>
-                    <OrangeIcon />
-                    <p>
-                        {data.orangeText}
-                    </p>
-                </div>
-                <div className={styles.elemWrapper}>
-                    <GreenIcon />
-                    <p>
-                        {data.greenText}
-                    </p>
-                </div>
-                <div className={styles.elemWrapper}>
-                    <PurpleIcon />
-                    <p>
-                        {data.purpleText}
-                    </p>
-                </div>
-                <div className={styles.elemWrapper}>
-                    <YellowIcon />
-                    <p>
-                        {data.yellowText}
-                    </p>
-                </div>
-                <div className={styles.elemWrapper}>
-                    <RedIcon />
-                    <p>
-                        {data.redText}
-                    </p>
-                </div>
+const MiddleComponent = ({ data }) => {
+  function getColorClass(color) {
+    switch (color) {
+      case "blue":
+        return styles.blue;
+      // Do rest of colors here
+      default:
+        break;
+    }
+  }
+  return (
+    <section className={styles.componentWrapper}>
+      <div className={styles.container}>
+        {/* Mapping through an array keeps code cleaner/more reusable. Allows you to add/remove items just by changing the JSON structure. This is a good practice to pick up. This is a DRY approach, do not repeat yourself. Make code reusable as much as posssible. */}
+        {data.textItems.map((item) => (
+          <div key={item.color} className={styles.elemWrapper}>
+            <div className={getColorClass(item.color)}>
+              {/*
+              Here I am importing one hexagon and using CSS to change the BG color, rather than importing multiple hexagons.
+              I would then import the symbols within the hexagon dynamically based on the JSON.
+               */}
+              <GlobalIcon />
             </div>
-        </section>
-    )
-}
+            <p>{item.text}</p>
+          </div>
+        ))}
+        {/* By mapping over content look how much HTML you can save. And then if you need to make changes you just change the one div above or the JSON file itself. No need to change 6 divs each time */}
+        {/* <div className={styles.elemWrapper}>
+          <BlueIcon />
+          <p>{data.blueText}</p>
+        </div>
+        <div className={styles.elemWrapper}>
+          <OrangeIcon />
+          <p>{data.orangeText}</p>
+        </div>
+        <div className={styles.elemWrapper}>
+          <GreenIcon />
+          <p>{data.greenText}</p>
+        </div>
+        <div className={styles.elemWrapper}>
+          <PurpleIcon />
+          <p>{data.purpleText}</p>
+        </div>
+        <div className={styles.elemWrapper}>
+          <YellowIcon />
+          <p>{data.yellowText}</p>
+        </div>
+        <div className={styles.elemWrapper}>
+          <RedIcon />
+          <p>{data.redText}</p>
+        </div> */}
+      </div>
+    </section>
+  );
+};
 
 export default MiddleComponent;
--- src/components/TopComponent.js
+++ src/components/TopComponent.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import styles from './TopComponent.module.css';
+import React from "react";
+import styles from "./TopComponent.module.css";
 
-const TopComponent = ({data}) => {
-    const {title, description} = data;
-    const topDescript = description.split(".")[0] + ".";
-    const botDescript = description.split(".")[1] + ".";
-    return (
-        <section className={styles.container}>
-            <h1 className={styles.title}>
-                {data.title}
-            </h1>
-            <p className={styles.description}>
-                {topDescript}
-            <br /><br />
-                {botDescript}
-            </p>
-        </section>
+const TopComponent = ({ data }) => {
+  const { title, descriptionArray } = data;
 
-    )
-}
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      {descriptionArray.map((text, index) => (
+        <p key={index} className={styles.description}>
+          {text}
+        </p>
+      ))}
+    </section>
+  );
+};
 
 export default TopComponent;
